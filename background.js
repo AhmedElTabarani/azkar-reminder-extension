8472e2b0e284e6ae8f6b7410a1839802b982a66b
AhmedEl-Tabarani <eltabaraniahmed@gmail.com>
2024-06-30 03:12:23 +0300

    Add Push Web Notification Random Duaa

--- background.js
+++ background.js
@@ -1,5 +1,40 @@
 import * as cache from './utils/cache.js';
 
-chrome.windows.onRemove.addListener(async (windowId) => {
-    await cache.clear();
- })
+// TODO: Add the rest of 100 duaa from the book and authentic sunnah
+// SEE https://nuqayah.com/f/100-duaa.pdf
+
+const filePath =
+  '/duaa/100-duaa-from-the-book-and-authentic-sunnah.json';
+setInterval(() => {
+  displayRandomDuaa();
+}, 1000 * 60 * 5); // TODO: change timeout to be customizable
+
+const getRandomDuaa = (data) => {
+  return data[Math.floor(Math.random() * data.length)];
+};
+
+const displayRandomDuaa = async () => {
+  const data = await cache.get(filePath);
+
+  if (data) {
+    const { duaa, category: title } = getRandomDuaa(data);
+    return pushWebNotificationRandomDuaa(title, duaa);
+  }
+
+  fetch(filePath)
+    .then((response) => response.json())
+    .then((data) => {
+      const { duaa, category: title } = getRandomDuaa(data);
+      pushWebNotificationRandomDuaa(title, duaa);
+      cache.set(filePath, data);
+    });
+};
+
+const pushWebNotificationRandomDuaa = async (title, duaa) => {
+  await chrome.notifications.create(null, {
+    type: 'basic',
+    iconUrl: '/icons/GitHub-Mark-32px.png', // TODO: change icon
+    title: title,
+    message: duaa.join('\n'),
+  });
+};
